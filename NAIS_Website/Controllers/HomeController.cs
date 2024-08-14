267ac211e347605cbbdfe89d85cef1321066db72
Farhad R <farxadjr@gmail.com>
2024-08-14 12:43:34 +0400

    first push
    .

---
+++ NAIS_Website/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NAIS_Website.Models;
+using System.Diagnostics;
+
+namespace NAIS_Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            var folderPath = Path.Combine(_webHostEnvironment.WebRootPath, "files/photos/carousel");
+            var imagePaths = Directory.GetFiles(folderPath)
+                          .Select(Path.GetFileName)
+                          .ToList();
+            ViewBag.ImagePaths = imagePaths;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
